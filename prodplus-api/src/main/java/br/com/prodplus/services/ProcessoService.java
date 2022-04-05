a5b45cce961a0d0c154e6c6c0e084b27b1c49d2f
Marlon F. Garcia <marlon_garcia@outlook.com>
2022-04-05 15:36:16 -0300

    Camada de serviço gerada

---
+++ prodplus-api/src/main/java/br/com/prodplus/services/ProcessoService.java
@@ -0,0 +1,13 @@
+package br.com.prodplus.services;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * 
+ * @author Marlon Fernando Garcia
+ *
+ */
+@Service
+public class ProcessoService {
+
+}
